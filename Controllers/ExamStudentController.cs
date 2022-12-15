64deba62077965e8721f39526749884b76616956
mohamed 18321692 <mohamed.18321692@compit.aun.edu.eg>
2022-12-15 20:49:21 +0200

    ExamStudent

--- Controllers/ExamStudentController.cs
+++ Controllers/ExamStudentController.cs
@@ -17,19 +17,31 @@ namespace examination_system.Controllers
             DB = new DB();
             var userid = User.Identity.GetUserId();
             var exam = DB.Exams.FirstOrDefault(e => e.Name == name);
-            if (DB.ExamStudent.Where(es => es.Exam.Id == exam.Id).Count() == 0)
+            var examstudent = DB.ExamStudent.Where(es => es.Exam.Id == exam.Id && es.Student.Id == userid);
+            if (examstudent.Count() == 0)
             {
+                Guid id = Guid.NewGuid();
                 DB.ExamStudent.Add(new ExamStudent
                 {
-                    Id = Guid.NewGuid(),
+                    Id = id,
                     Exam = exam,
                     Student = DB.Users.FirstOrDefault(u => u.Id == userid)
                 });
                 DB.SaveChanges();
+                ViewBag.ExamStudentId = id;
+            }
+            else {
+                ViewBag.ExamStudentId = examstudent.FirstOrDefault().Id;
             }
             if(DateTime.Now > exam.Date && DateTime.Now < exam.Date.AddMinutes(exam.Duration))
                 return View(exam);
             return View();
+        }
+        [HttpPost, ValidateAntiForgeryToken, Authorize(Roles = "superadmin,student")]
+        public bool AddAnswer() {
+            DB = new DB();
+            DB.ExamStudent.
+            return true;
         }
     }
 }
